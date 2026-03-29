228926d1d80831de82fd8b7a039930c17a5b4e47
Pablo <pablo@myo.de>
2026-03-30 00:59:47 +0200

    fix paths and images 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arar.com.co',
-  base: '/arar-landing-page',
+  base: '/arar-landing-page/',
   output: 'static',
   build: {
     assets: 'assets',
