2196452d6a6724d941adf6d672c1de21c4f57db2
Pablo <pablo@myo.de>
2026-04-30 22:42:43 +0200

    stashed herocanvas and change of site name config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://arar.com.co',
+  site: 'https://pabluchenkooo.github.io',
   base: '/arar-landing-page/',
   output: 'static',
   build: {
